9731c6f6e2e380c5ebe20937bed01922162037d6
seongho2 <125865161+seongho2@users.noreply.github.com>
2024-01-18 01:33:54 +0900

    Add files via upload

---
+++ 20240118/yaw_control.cpp
@@ -0,0 +1,83 @@
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
+#include "geometry_msgs/Twist.h"
+#include "sensor_msgs/Imu.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Matrix3x3.h"
+
+#define DEG2RAD(x) (M_PI / 180.0) * x
+#define RAD2DEG(x) (180.0 / M_PI) * x
+
+float yaw_error_old = 0.0;
+double roll, pitch, yaw;
+double heading_yaw = DEG2RAD(45.0);
+
+void imu1Callback(const sensor_msgs::Imu::ConstPtr& msg) 
+{
+    tf2::Quaternion q(
+        msg->orientation.x,
+        msg->orientation.y,
+        msg->orientation.z,
+        msg->orientation.w);
+
+    tf2::Matrix3x3 m(q);      
+    m.getRPY(roll, pitch, yaw);
+    printf("%f\n", RAD2DEG(yaw));
+}
+
+void yaw_control(geometry_msgs::Twist &cmd_vel)
+{
+    float Kp = 0.5;
+    float Ki = 0.0;
+    float Kd = 0.8;
+    float yaw_error = 0.0;
+    float yaw_error_d = 0.0;
+    float Steering_Angle = 0.0;
+
+    yaw_error = heading_yaw - yaw;
+    yaw_error_d = yaw_error - yaw_error_old;
+    Steering_Angle = Kp * yaw_error + Kd * yaw_error_d + Ki * yaw_error;
+    
+    cmd_vel.linear.x = 1.0;
+    cmd_vel.angular.z = Steering_Angle;
+
+    if (yaw_error >= M_PI)  yaw_error = yaw_error - 2 * M_PI;
+    if (yaw_error <= -M_PI)  yaw_error = yaw_error + 2 * M_PI;
+
+    yaw_error_old = yaw_error; 
+}
+
+int main(int argc, char **argv)
+{
+    geometry_msgs::Twist cmd_vel;
+
+    ros::init(argc, argv, "yaw_control");
+
+    ros::NodeHandle n;
+
+    ros::Subscriber yaw_control_sub = n.subscribe("/imu", 1000, imu1Callback);
+    ros::Publisher yaw_cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/ackermann_steering_controller/cmd_vel", 1000);
+    ros::Rate loop_rate(30.0);
+
+    int count = 0;
+    while (ros::ok())
+    {  
+        yaw_control(cmd_vel);
+        if (fabs(heading_yaw - yaw) > 0.01)
+        {
+            yaw_cmd_vel_pub.publish(cmd_vel);
+        }
+        else
+        {
+            cmd_vel.angular.z = 0.0;
+            cmd_vel.linear.x = 0.0;
+            yaw_cmd_vel_pub.publish(cmd_vel);
+        }  
+
+        ros::spinOnce();
+        loop_rate.sleep();
+        ++count;
+    }
+
+    return 0;
+}
